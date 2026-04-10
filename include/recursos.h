f140fe635c92b69b30efb2eb65d9f0ea4419ae4e
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2026-04-10 12:51:35 -0600

    Add recursos: Semaforo para controlar acceso a los recursos compartidos (Muelles)

---
+++ include/recursos.h
@@ -0,0 +1,32 @@
+#ifndef RECURSOS_H
+#define RECURSOS_H
+ 
+#include <semaphore.h>
+
+/*
+
+Notas: 
+Recursos compartidos entre los hilos (camiones) y el planificador: Controla el acceso a los muelles de carga
+Encapsula el semáforo que controla el acceso a los muelles.
+Se inicializa con la cantidad de muelles disponibles (ej. 3).
+
+sem_wait: Toma un muelle (decrementa el semáforo).
+sem_post: Libera un muelle (incrementa el semáforo).
+
+*/
+
+typedef struct
+{
+    sem_t semaforo_muelles; 
+    int num_muelles;
+
+} RecursosCompartidos;
+
+void inicializar_recursos(RecursosCompartidos *recursos, int num_muelles);
+void tomar_muelle(RecursosCompartidos *recursos);
+void liberar_muelle(RecursosCompartidos *recursos);
+void destruir_recursos(RecursosCompartidos *recursos);
+
+
+#endif
+
